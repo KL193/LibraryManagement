c49f6226fd69149cfc40940d40c7a79321a9d522
KL193 <lakshankasthuriarachchi@gmail.com>
2025-01-12 10:17:08 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/libraraymanagement/LibrarayManagementApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.libraraymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarayManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarayManagementApplication.class, args);
+    }
+
+}
